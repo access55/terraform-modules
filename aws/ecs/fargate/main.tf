6dbf9eeabfe3faa2e1c931489da632fd3ea18b0a
Marcos Gabriel <mgabriel@wvinicima01.casa.lan>
2021-04-23 12:09:29 -0300

    Fargate Cluster

--- aws/ecs/fargate/main.tf
+++ aws/ecs/fargate/main.tf
@@ -3,7 +3,6 @@
 #------------------------------------------------------------------------------
 resource "aws_ecs_cluster" "cluster" {
   name                  = var.name
-  tags                  = var.tags
     capacity_providers = ["FARGATE_SPOT", "FARGATE"]
 
   default_capacity_provider_strategy {
