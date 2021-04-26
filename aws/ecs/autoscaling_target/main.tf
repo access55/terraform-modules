b06cb41ae006bad1f3ff9245719e8ab974b299d1
Marcos Gabriel <mgabriel@wvinicima01.casa.lan>
2021-04-26 13:15:10 -0300

    add more options cloudflare Options to dns record

--- aws/ecs/autoscaling_target/main.tf
+++ aws/ecs/autoscaling_target/main.tf
@@ -1,7 +1,7 @@
 resource "aws_appautoscaling_target" "app_scale_target" {
   service_namespace  = "ecs"
   resource_id        = "service/${var.name}/${var.app}"
-  scalable_dimension = "ecs:service:DesiredCount"
+  scalable_dimension = "${var.scalable_dimension}"
   max_capacity       = var.ecs_autoscale_max_instances
   min_capacity       = var.ecs_autoscale_min_instances
 }
